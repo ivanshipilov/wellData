18b345db0693197dd64c45e6478acf47112b2dca
ivan.shipilov88 <ivan.shipilov88@gmail.com>
2018-07-28 15:43:45 +0300

    Добавьте файлы проекта.

---
+++ Well2/MainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace well
+{
+    public partial class MainForm : Form
+    {
+        // создание List со скважинами в проекте  
+        List<Well> listWells = new List<Well>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //Load += MainForm_Load;
+            //Panel p2 = splitContainer1.Panel2;
+            //p2.Paint += new PaintEventHandler(Panel2Draw);
+        }
+
+        //private void Panel2Draw(object sender, PaintEventArgs e)
+        //{
+        //    Graphics g = e.Graphics;
+        //    Panel p = sender as Panel;
+
+        //    Pen pen = new Pen(Brushes.Green, 20f);
+        //    Point p1 = new Point(10, 10);
+        //    Point p2 = new Point(this.Width, this.Height);
+        //    g.DrawLine(pen, p1, p2);
+        //}
+
+        //private void MainForm_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Graphics g = Graphics.FromHwnd(Handle);
+            //Pen pen = new Pen(Brushes.Green, 20f);
+            //Point p1 = new Point(10,10);
+            //Point p2 = new Point(this.Width, this.Height);
+            //g.DrawLine(pen, p1, p2);
+        }
+
+        //private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        // открытие нового файла из главного меню
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // проверка, если нажата отмена
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            else
+            {
+                //открытие файла - перенести в метод well?
+                string filePath = openFileDialog1.FileName;
+                string fileContent = System.IO.File.ReadAllText(filePath);
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                //создается новая скважина                  
+                Well WellNew = new Well(fileName, filePath);
+
+                //скважина добавляется в список скважин - пока не используется
+                listWells.Add(WellNew);
+
+                //в дерево скважин добавляется новая скважина   
+                wellsTree.Nodes.Add(listWells[Well.count - 1].WellName);
+                //добавление методов в скважину (в дереве)
+                foreach (string method in WellNew.WellMethods(filePath))
+                {
+                    wellsTree.Nodes[Well.count - 1].Nodes.Add(method);
+                }
+                wellsTree.ExpandAll();
+
+
+                //List<decimal> temp = new List<decimal>();
+                //temp = 
+
+                //MessageBox.Show(WellNew.WellData(filePath).Keys.First());
+
+                // MessageBox.Show(WellNew.WellData(filePath).Count.ToString());
+
+
+
+
+                string сhartName = WellNew.WellName + Well.count;
+                Chart chartNew = new Chart();
+                chartNew.Parent = splitContainer1.Panel2;
+                // chartNew.Dock = DockStyle.Fill;
+                chartNew.ChartAreas.Add(new ChartArea(сhartName));
+                Series ser1 = new Series(сhartName);
+                ser1.ChartType = SeriesChartType.Line;
+                ser1.ChartArea = сhartName;
+                string depthCol = WellNew.WellData(filePath).Keys.First();
+
+                for (int i = 0; i < WellNew.WellData(filePath).Count; i++) //fix всего 6 значений
+                {
+                    foreach (decimal depthValue in WellNew.WellData(filePath)[depthCol])
+                    {
+                        int x = 0; decimal y = depthValue;
+                        ser1.Points.AddXY(x, y);
+                    }
+                 //   chartNew.Series.Add(ser1);
+                }
+
+                //for (double x = -Math.PI; x <= Math.PI; x += Math.PI / 10.0)
+                //{
+                //    ser1.Points.AddXY(x, Math.Sin(x));
+                //}
+                //chartNew.Series.Add(ser1);
+                //foreach (KeyValuePair<string, List<decimal>> pair in WellNew.WellData(filePath))
+                //{
+                //foreach (decimal val in pair.Value)
+                //{ s =/* s +*/ "key: " + pair.Key + "val= " + val + Environment.NewLine; }
+                //}
+                // MessageBox.Show(s);
+
+
+
+
+            }
+        }        
+    }
+}
---
+++ Well2/Well.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace well
+{
+    public class Well
+    {
+        public static int count = 0;
+        private string wellName;
+        public string WellName { get { return wellName; } }
+        private string wellPath;
+        //Dictionary<string, List<decimal>> methodsDict = new Dictionary<string, List<decimal>>();
+        List<string> methodsList = new List<string>();
+        Dictionary<string, List<decimal>> dataDict = new Dictionary<string, List<decimal>>();
+
+        //constructor
+        public Well(string wellName, string wellPath)
+        {
+            this.wellName = wellName;
+            this.wellPath = wellPath;
+            GetWellMethodsFromFile(wellPath);
+            GetWellDataFromFile(wellPath);
+            Well.count++;
+        }
+
+        public void Draw()
+        {
+            Pen pen = new Pen(Brushes.Green);
+        }
+
+        //parse well methods
+        public void GetWellMethodsFromFile(string wellPath)
+        {
+            StreamReader reader = new StreamReader(wellPath);
+            int counter = 0;
+            int numLine = 1;
+            int methodsCount = 0;
+            string line;
+            string curve = "~C";
+            string param = "~P";
+            string hash = "#";
+            bool curveFinded = false;
+            //List<string> methods = new List<string>();
+            while ((line = reader.ReadLine()) != null)
+            {
+                counter++;
+                if (line.StartsWith(curve))
+                {
+                    curveFinded = true;
+                    numLine = counter;
+                    continue;
+                }
+                if (curveFinded && line.StartsWith(hash) && ((counter - numLine) == 1))
+                {
+                    numLine++; continue;
+                }
+                else
+                {
+                    if (curveFinded && !line.StartsWith(param))
+                    {
+                        string[] word = line.Split(new string[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+                        this.methodsList.Add(word[methodsCount]);
+                        methodsCount += methodsCount;
+                    }
+                    else
+                    {
+                        if (line.StartsWith(param))
+                        {
+                            break;
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+
+        //parse well data
+        public Dictionary<string, List<decimal>> GetWellDataFromFile(string wellPath)
+        {
+            int counter = 0;
+            string line;
+            int numLine = 1;
+            int countValues = 0;
+            string ascii = "~A";
+            string hash = "#";
+            bool asciiFinded = false;
+            List<string> methodsList = new List<string>();
+
+            //запись в лист с методами методов скважины из функции WellMethods
+            foreach (string method in this.methodsList)
+            {
+                methodsList.Add(method);
+            }
+
+            //StreamReader reader1 = null; 
+            //try
+            //{
+            //    reader1 = new StreamReader(wellPath);
+            //    string s = wellName;
+            //    WellMethods(wellPath);
+
+            //    throw new Exception();
+            //}
+            //catch(Exception ex)
+            //{
+            //    System.Windows.Forms.MessageBox.Show(ex.ToString());
+            //}
+            //finally
+            //{
+            //    System.Windows.Forms.MessageBox.Show("Test 123");
+            //    if(reader1 != null)
+            //    {
+            //        reader1.Close();
+            //    }
+            //}
+
+            using (StreamReader reader = new StreamReader(wellPath))
+            {//fix
+                while ((line = reader.ReadLine()) != null)
+                {
+                    counter++;
+                    if (line.StartsWith(ascii))
+                    {
+                        asciiFinded = true;
+                        numLine = counter;
+                        continue;
+                    }
+                    if (asciiFinded && line.StartsWith(hash) && ((counter - numLine) == 1))
+                    {
+                        numLine++;
+                        continue;
+                    }
+                    else
+                    {
+                        if (asciiFinded)
+                        {
+                            string[] word = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                            int methodNum = 0;
+                            foreach (string wrd in word)
+                            {
+                                decimal val;
+                                decimal.TryParse(wrd, NumberStyles.Any, CultureInfo.InvariantCulture, out val);
+
+                                if (countValues == 0)
+                                {
+                                    dataDict.Add(methodsList[methodNum], new List<decimal>());
+                                    dataDict[methodsList[methodNum]].Add(val);
+                                }
+                                else
+                                {
+                                    dataDict[methodsList[methodNum]].Add(val);
+                                }
+                                methodNum++;
+                            }
+                            countValues++;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            return dataDict;
+        }
+
+        //show well methods
+        public List<string> WellMethods(string wellPath)
+        {
+            return methodsList;
+        }
+
+        //show well data
+        public Dictionary<string, List<decimal>> WellData(string wellPath)
+        {
+            return dataDict;
+        }
+    }
+}
